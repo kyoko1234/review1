9504f386570fc119a13267c19913c2a82d7762e9
kyoko1234 <matsuko1014@yahoo.co.jp>
2012-08-09 20:24:44 +0900

    second commit

---
+++ src/Person.java
@@ -0,0 +1,59 @@
+public class main Person(){
+
+	public Person() = new Person();
+
+	String uji =  String("氏");
+	String na = String("名");
+
+	String firstname = this.String();
+	String lastname = this.String();
+
+	//変数na取得
+	public void getFirstName(String na){
+
+		
+
+	}
+
+	//変数naをfirstnameに代入
+	public setFirstName(String getFirstName()){
+
+		firstname = na;
+
+	}
+
+	//変数uji取得
+	public void getLastName(String uji){
+
+		
+
+	}
+
+	//変数lastnameにuji代入
+	public setFirstname(String getLastName()){
+
+		lastname = uji;
+
+	}
+
+	//showNameメソッドは取得したものを表示するメソッド
+	public void showName(){
+		
+		firstname = setFirstName("高志")；
+		lastname = setastName("渡辺");
+
+		System.out.println("氏："lastname "名:"firstname);
+
+		firstname = setirstName("充史")；
+		lastname =  setLastName("和田");
+
+		System.out.println("氏："lastname "名:"firstname);
+
+		firstname = setFirstName("高志")；
+		lastname = setLastName("渡辺");
+
+		System.out.println("氏："lastname "名:"firstname);
+
+	}
+
+}
